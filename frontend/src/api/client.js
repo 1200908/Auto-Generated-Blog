d31ab15f0f40ef8c2ee17a84da8774b8d3613dca
1200908 <1200908@isep.ipp.pt>
2025-12-07 21:31:16 +0000

    docker

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://16.171.12.82:8080';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
